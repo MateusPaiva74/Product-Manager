07a4c6747c078e26f9e68edd1a136b5729f72a9d
Mateus Andre Paiva de Araujo <130513304+MateusPaiva74@users.noreply.github.com>
2025-03-21 21:33:58 -0300

    testes e correções

--- product_manager/src/pages/supplier/SupplierList.jsx
+++ product_manager/src/pages/supplier/SupplierList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../../api/index'
 import { Link } from 'react-router-dom'
-import { FaPlus, FaTrash, FaEdit, FaExclamationTriangle } from 'react-icons/fa'
+import { FaPlus, FaTrash, FaEdit, FaExclamationTriangle, FaCheckCircle } from 'react-icons/fa'
 import Modal from 'react-modal'
 
 const SupplierList = () => {
@@ -9,13 +9,15 @@ const SupplierList = () => {
     const [supplier, setSupplier] = useState([])
     const [selectedSupplier, setSelectSupplier] = useState(null)
     const [openModal, setOpenModal] = useState(false)
+    const [modalSuccessOpen, setModalSuccessOpen] = useState(false)
   
     useEffect(() => {
         const findSupplier = () =>{
             axios.get('/suppliers')
             .then(response => {
                 setSupplier(response.data)
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.error("Ocorreu um erro ao buscar os fornecedores", error)
             })
         }
@@ -28,16 +30,20 @@ const SupplierList = () => {
     }
     const closeModal = () => {
         setOpenModal(false)
+        setSelectSupplier(null)
+    }
+    const openSuccessModal = () => {
+        setModalSuccessOpen(true)
+        setTimeout(() => setModalSuccessOpen(false), 2000)
     }
     const removeSupplier = () => {
         axios.delete(`/suppliers/${selectedSupplier.id}`)
         .then(() => {
-            setSupplier(prevSupplier => prevSupplier.filter(supplier => supplier.id))
+            setSupplier(prevSupplier => prevSupplier.filter(supplier => supplier.id !== selectedSupplier.id))
             closeModal()
-        }).catch(error => {
-            console.error("Ocorreu um erro ao deletar o fornecedor", error)
+            openSuccessModal()
         })
-
+    }
     return (
     <div className="container mt-5">
         <h2 className="mb-4">List of Suppliers</h2>
@@ -54,35 +60,54 @@ const SupplierList = () => {
                 </tr>
             </thead>
             <tbody>
-                {supplier.map(supplier => (
-                <tr key={supplier.id}>
-                    <td>{supplier.name}</td>
-                    <td>{supplier.cnpj}</td>
-                    <td>{supplier.email}</td>
-                    <td>
-                        <Link to={`/edit-supplier/${supplier.id}`} className="btn btn-sm btn-warning"><FaEdit className="icon icon-btn"/>Edit
-                        </Link>
-                        <button onClick={()=>openModal(supplier)} className="btn btn-sm btn-danger"><FaTrash className="icon icon-btn" />Delete
-                        </button>
-                    </td>
-                </tr>))}
+                    {supplier.map(supplier => (
+                    <tr key={supplier.id}>
+                        <td>{supplier.name}</td>
+                        <td>{supplier.cnpj}</td>
+                        <td>{supplier.email}</td>
+                        <td>
+                            <Link to={`/edit-supplier/${supplier.id}`} className="btn btn-sm btn-warning">
+                                <FaEdit className="icon icon-btn"/>Edit
+                            </Link>
+                            <button onClick={()=>openModal(supplier)} className="btn btn-sm btn-danger">
+                                <FaTrash className="icon icon-btn" />Delete
+                            </button>
+                        </td>
+                    </tr>
+                    ))
+                    }
             </tbody>
         </table>
         <Modal
             isOpen={openModal}
             onRequestClose={closeModal}
             className="modal"
-            overLayClassName="overlay"
+            overlayClassName="overlay"
         >
-            <FaExclamationTriangle className="icon"/>
-            <h2>Confirm Deletion</h2>
-            <p>Are you sure you want to delete the supplier {selectedSupplier && selectedSupplier.name}?</p>
-            <div className="modalButtons">
-                <button onClick={closeModal} className="btn btn-secondary"> Cancel</button>
+            <div className="modalContent">
+                <FaExclamationTriangle className="icon"/>
+                <h2>Confirm Deletion</h2>
+                <p>Are you sure you want to delete the supplier
+                    {selectedSupplier && selectedSupplier.name}?</p>
+                <div className="modalButtons">
+                    <button onClick={closeModal} className="btn btn-secondary"> Cancel</button>
+                    <button onClick={removeSupplier} className="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </Modal>
+
+        <Modal
+            isOpen={modalSuccessOpen}
+            onRequestClose={() => setModalSuccessOpen(false)}
+            className="modal"
+            overlayClassName="overlay"
+        >
+            <div className="modalContent">
+                <FaCheckCircle className="icon successIcon"/>
+                <h2>Supplier deleted successfully!</h2>
             </div>
         </Modal>
     </div>
   )
-}
 }
 export default SupplierList
